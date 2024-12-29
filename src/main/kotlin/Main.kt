1b2b326e8fd48030d6d8b571c2bb9aa901a98a16
Allex-Nik <alexandr.nikolaev96@gmail.com>
2024-12-29 23:46:40 +0100

    Added initial input handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,73 +8,56 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import org.example.github.*
+import org.example.models.Repository
 
+
+object Constants {
+    const val DEFAULT_BASE_BRANCH = "main"
+    const val DEFAULT_HEAD_BRANCH = "hello"
+    const val DEFAULT_FILE_NAME = "Hello.txt"
+    const val DEFAULT_FILE_CONTENT = "Hello World"
+    const val DEFAULT_PR_TITLE = "Add Hello.txt"
+    const val DEFAULT_PR_DESCRIPTION = "Added Hello.txt with Hello world"
+}
 
 /**
  * The entrance point of the program.
  */
 suspend fun main() {
-
-//    // Load the token
-//    val config = loadConfig().getOrElse {
-//        println("Failed to load config.json. Error: ${it.message}")
-//        return
-//    }
-
 //    println("GitHub token: ${config.githubToken}")
 
-    // Create a client
-    val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json()
-        }
-        install(DefaultRequest) {
-//            header("Authorization", "Bearer ${config.githubToken}")
-            accept(ContentType.Application.Json)
-        }
-    }
+    val baseBranch = handleInput(
+        "Enter the name of the base branch", Constants.DEFAULT_BASE_BRANCH)
+
+    val headBranch = handleInput(
+        "Enter the name of the head branch", Constants.DEFAULT_HEAD_BRANCH)
+
+    val fileName = handleInput(
+        "Enter the name of the file you want to add", Constants.DEFAULT_FILE_NAME)
+
+    val content = handleInput(
+        "Enter the content of the file you want to add", Constants.DEFAULT_FILE_CONTENT )
+
+    val prTitle = handleInput(
+        "Enter the title of your pull request", Constants.DEFAULT_PR_TITLE)
+
+    val prDescription = handleInput(
+        "Enter the description of your pull request", Constants.DEFAULT_PR_DESCRIPTION)
 
 //    val userInfo: String = client.get("https://api.github.com/user").bodyAsText()
 //    println("User info: $userInfo")
 
+    val client = createHttpClient()
     try {
         // Get the list of repositories
-        val repositories = interactiveGetRepositories(client)
-        if (repositories == null) {
-            println("Exiting program.")
-            return
-        }
-
-        //    repositories.forEach { repo ->
-        //        println("Name: ${repo.name}, URL: ${repo.html_url}, Private: ${repo.private}")
-        //    }
-
-        // Print the list of the repositories
-        println("Available repositories: ${repositories.size}")
-        repositories.forEachIndexed { index, repository ->
-            println("${index + 1}. Name: ${repository.name}, URL: ${repository.html_url}, Private: ${repository.private}")
-        }
-
-        // Ask the user to choose a repository and save the choice
-        println("Enter the number of the repository: ")
-        val selectedIndex = readlnOrNull()?.toIntOrNull()
-
-        // Check the input
-        if (selectedIndex == null || selectedIndex !in 1..repositories.size) {
-            println("Invalid selected repository index. Try again.")
-            return
-        }
-
-        val selectedRepository = repositories[selectedIndex - 1]
-        println("Selected repository: ${selectedRepository.name}")
-
+        val selectedRepository = selectRepository(client) ?: return
 
         val finalBranchName = interactiveCreateBranch(
             client,
             selectedRepository.owner.login,
             selectedRepository.name,
-            "test",
-            "main"
+            headBranch,
+            baseBranch
         )
         if (finalBranchName == null) {
             return
@@ -85,8 +68,8 @@ suspend fun main() {
             selectedRepository.owner.login,
             selectedRepository.name,
             finalBranchName,
-            "Hello.txt",
-            content = "Hello world"
+            fileName,
+            content
         )
         if (!fileAdded) {
             return
@@ -96,10 +79,10 @@ suspend fun main() {
             client,
             selectedRepository.owner.login,
             selectedRepository.name,
-            title = "Add Hello.txt",
-            body = "Added Hello.txt with Hello world",
+            title = prTitle,
+            body = prDescription,
             headBranch = finalBranchName,
-            baseBranch = "main"
+            baseBranch = baseBranch
         )
         if (!prCreated) {
             return
@@ -109,4 +92,43 @@ suspend fun main() {
     } finally {
         client.close()
     }
+}
+
+fun createHttpClient() = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        json()
+    }
+    install(DefaultRequest) {
+        accept(ContentType.Application.Json)
+    }
+}
+
+suspend fun selectRepository(client: HttpClient): Repository? {
+    val repositories = interactiveGetRepositories(client) ?: return null
+
+    // Print the list of the repositories
+    println("Available repositories: ${repositories.size}")
+    repositories.forEachIndexed { index, repository ->
+        println("${index + 1}. Name: ${repository.name}, URL: ${repository.html_url}, Private: ${repository.private}")
+    }
+
+    // Ask the user to choose a repository and save the choice
+    println("Enter the number of the repository: ")
+    val selectedIndex = readlnOrNull()?.toIntOrNull()
+
+    // Check the input
+    if (selectedIndex == null || selectedIndex !in 1..repositories.size) {
+        println("Invalid selected repository index. Try again.")
+        return null
+    }
+
+    return repositories[selectedIndex - 1].also { println("Selected repository: ${it.name}") }
+}
+
+fun handleInput(message: String, default: String): String {
+    println("$message. Default: $default")
+    return readlnOrNull()
+        ?.trim()
+        ?.takeIf { it.isNotEmpty() }
+        ?: default
 }
